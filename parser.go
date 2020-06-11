5469abebd6999c8ad218b0195719c7ec966e7df2
sergeevem2007 <34450912+sergeevem2007@users.noreply.github.com>
2020-06-11 12:49:08 +0400

    Add files via upload

--- parser.go
+++ parser.go
@@ -5,38 +5,68 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
+	parseInput()
+	a := parseInput()
+	fmt.Println(a)
+	for _, elem := range parseInput() {
+		parsePage(elem)
+	}
+	// linkPage := "https://www.google.com/"
+	// parsePage(linkPage)
+}
+
+func parseInput() []string {
 	input, err := os.Open("input.txt")
+	defer input.Close()
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(input)
 
-	linkPage := "https://www.google.com/"
-	parsePage(linkPage)
+	inputContent, err := ioutil.ReadAll(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return strings.Split(string(inputContent), "\r\n")
 }
 
 func parsePage(link string) {
-	page, err := http.Get(link)
+	page, err := http.Get("http://" + link)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	pageContent, err := ioutil.ReadAll(page.Body)
+	defer page.Body.Close()
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer page.Body.Close()
 
-	file, err := os.Create("link.html")
+	err = os.Mkdir(link, 1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	file, err := os.Create(link + ".html")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	defer file.Close()
 	file.Write(pageContent)
 	return
 }
